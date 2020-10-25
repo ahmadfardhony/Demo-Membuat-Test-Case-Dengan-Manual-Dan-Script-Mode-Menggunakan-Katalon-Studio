57f3aafdfbab544b0a5bdd4a248eef40254391fb
LENOVO <LENOVO@DESKTOP-4V7KC2B>
2020-10-25 19:36:00 +0700

    Fist Commit

---
+++ Test Suites/Regression Test.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>Achmadfardhony@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3948b8a0-0065-44c4-b8a4-e09b9a0cc168</testSuiteGuid>
+   <testCaseLink>
+      <guid>f9a242d2-ae75-4a7b-949b-993ae42e0471</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01-Login Berhasil dengan Username dan Password yang Valid (Script Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f83ca40-8da2-4d79-8f8c-4b8feea1cf78</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02-Gagal Login dengan Username dan Password InValid (Manual Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0a4e87a6-47ef-45b9-9ddc-711c1ed8e024</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC03-Gagal Login dengan Username Valid Dan Password InValid (Script Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d0cbe62f-8c1d-4c55-8b52-a40b5e697b43</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC04-Gagal Login dengan Username Invalid Dan Password Valid (Script Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>08043e96-63c3-49ce-83fc-4d87ba69d34d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC05-Gagal Login dengan hanya menginputkan username saja (Script Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8b31febe-5531-4752-98c3-0cefffe1549f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC06-Gagal Login dengan hanya menginputkan Password saja (Script Mode)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>978abeb0-56eb-488c-b886-af889267e8dc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC07-Gagal Login dengan tidak menginput Username dan password (Script Mode)</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
